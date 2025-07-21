ee995493401604a0f29c5a6a48679749be4aa87f
Zachariah Dellimore <42615901+Party14534@users.noreply.github.com>
2025-07-21 13:57:01 -0400

    Added 16 bit load tests

---
+++ tests/loadTests/16BitLoadTests.cpp
@@ -0,0 +1,35 @@
+#include "gtest/gtest.h"
+#include "../../src/Gameboy/gameboy.h"
+
+TEST(Inc16BitTest, HandlesBCPair) {
+    Gameboy g("");
+
+    // Set instruction
+    g.mem[0x0000] = 0x03; // INC BC
+
+    // Set register values
+    short unsigned int val = 100;
+    g.r.setBC(val);
+
+    // Run Code
+    g.FDE();
+
+    // Assert value is correct
+    assert(g.r.getBC() == (unsigned short int)101);
+}
+
+TEST(Inc16BitTest, HandlesSP) {
+    Gameboy g("");
+
+    // Set instruction
+    g.mem[0x0000] = 0x33; // INC SP
+
+    // Set register values
+    g.SP = 100;
+
+    // Run Code
+    g.FDE();
+
+    // Assert value is correct
+    assert(g.SP == (unsigned short int)101);
+}
